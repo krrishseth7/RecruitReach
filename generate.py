360732fe39e8f3b00146c52d9c23f1b160feb754
Krrish Seth <sethkrri@msu.edu>
2025-06-21 01:54:21 -0400

    added lines

--- generate.py
+++ generate.py
@@ -3,3 +3,18 @@ import requests
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 from openai import OpenAI
+import msoffcrypto
+import io
+import openpyxl
+load_dotenv(override=True)
+api_key=os.getenv('OPENAI_API_KEY')
+if not api_key:
+    print("No API key was found - please head over to the troubleshooting notebook in this folder to identify & fix!")
+elif not api_key.startswith("sk-proj-"):
+    print("An API key was found, but it doesn't start sk-proj-; please check you're using the right key - see troubleshooting notebook")
+elif api_key.strip() != api_key:
+    print("An API key was found, but it looks like it might have space or tab characters at the start or end - please remove them - see troubleshooting notebook")
+else:
+    print("API key found and looks good so far!")
+openai=OpenAI()
+message=[{"role":"system","content":"You are a very professional candidate, writing from my POV, my name is Krrish"},{"role":"user","content":"Write an email as a cold email, you have access to the name, email, company,  position etc of the person, I am internship less so need an internship, make perzonalized email for each person"}]
